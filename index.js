19862f17048857af51b39f54e91ba1f86db1d15f
Spencer Mathews <smathews0906@gmail.com>
2022-06-07 16:07:10 -0500

    commit tree 6f27c61915d2a045afb9aa37dd9d255fb0909e38

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i=0; i< gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      }
+    
+      return gifts;
+    }
+    
+    wrapGifts(gifts)
+
+    
+
+    function writeCards(name, event){
+      const cardsArray = [];
+        for(let a=0; a < name.length; a++){
+            const message = (`Thank you, ${name[a]}, for the wonderful ${event} gift!`);
+            cardsArray.push(message);
+            
+        }
+        return(cardsArray)
+    }
+
+    console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+    //while loop
+    
+
+
+    function countDown(n){
+        while ( n >= 0) {
+          console.log(n);
+        n=n-1;
+        }
+    }
+    countDown(21)
